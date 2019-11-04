b92cd9d09b6d41636d9d184df7be6b20776d6720
wallog <wanglong.1218@163.com>
2019-11-04 16:45:54 +0800

    fix

--- modHDFS.py
+++ modHDFS.py
@@ -20,10 +20,10 @@ class interHDFS:
     def apiVersion(self):
         return "v1"
 
-    def listdir(self, dirname:str='/'):
+    def listDir(self, dirname:str='/'):
         return self.connect.list(dirname)
 
-    def getfiles(self, dirname:str, depth:int=0) -> list:
+    def getFiles(self, dirname:str, depth:int=0) -> list:
         l = []
         if not dirname:
             print("dirname is null")
@@ -34,12 +34,23 @@ class interHDFS:
                         l.append(file[0]+'/'+f)
             return l
 
-    def gencsv(self, filename:str):
+    def downloadToCsv(self, filename:str) -> None:
         '''only split for the '€€' sign, and generate same filename in current directory'''
         with self.connect.read(filename, encoding='utf-8') as reader:
-            with open(filename.split('/')[-1].split('.')[0]+'.csv', 'w+') as cf:
+            with open(csvdir+filename.split('/')[-1].split('.')[0]+'.csv', 'a+') as cf:
                 for line in reader.readlines():
                     newline = line.replace('€€', ',')
                     cf.write(newline)
 
+class uploadHDFS(interHDFS):
+    '''default directory is "/home"'''
+    def __init__(self, url, csvdir='/home', user=None, **kwargs):
+        super().__init__(url, user, csvdir='home')
+        self.csvdir = csvdir
+
+    def upload(self, hdfs_path):
+        try:
+            self.connect.upload(hdfs_path, self.csvdir)
+        except Exception as e:
+            print(f"[ERROR]:{e}")
 
